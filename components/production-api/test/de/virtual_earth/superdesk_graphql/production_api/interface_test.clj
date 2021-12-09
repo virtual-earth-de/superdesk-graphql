1a54b7f0edf05ef165c7be5731797371f1b95947
Mathias Picker <Mathias.Picker@virtual-earth.de>
2021-12-09 18:25:03 +0100

    add data.json to production-api, also change forgotten test namespace

--- components/production-api/test/de/virtual_earth/superdesk_graphql/production_api/interface_test.clj
+++ components/production-api/test/de/virtual_earth/superdesk_graphql/production_api/interface_test.clj
@@ -1,6 +1,6 @@
-(ns de.virtual-earth.superdesk-graphql.production-api-auth.interface-test
+(ns de.virtual-earth.superdesk-graphql.production-api.interface-test
   (:require [clojure.test :as test :refer :all]
-            [de.virtual-earth.superdesk-graphql.production-api-auth.interface :as production-api-auth]))
+            [de.virtual-earth.superdesk-graphql.production-api.interface :as production-api]))
 
 (deftest dummy-test
   (is (= 1 1)))
